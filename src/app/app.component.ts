b6006d8cb6a9f35e6fbe7809c3931d2afe21e899
Ayman <aymanuddin1111.au@gmail.com>
2023-08-29 22:30:50 +0530

    ngx-toaster added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { setTheme } from 'ngx-bootstrap/utils';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [RouterOutlet]
+  imports: [RouterOutlet]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mmj_store';
-  constructor() {
-    setTheme('bs5');
+  constructor(private toaster: ToastrService) {
+        setTheme('bs5');
+  }
+  ngOnInit() {
+    this.toaster.success("Welcome to MMJ")
+
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,24 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastrModule, provideToastr } from 'ngx-toastr';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, ToastrModule.forRoot({
+    timeOut: 10000,
+    positionClass: 'toast-bottom-right',
+    preventDuplicates: true,
+    progressBar: true,
+    closeButton: true,
+    newestOnTop: true,
+    tapToDismiss: true,
+    autoDismiss: true,
+  }),
+  ),
+    provideAnimations(), // required animations providers
+    provideToastr(), // Toastr providers
+  ]
 })
   .catch(err => console.error(err));
