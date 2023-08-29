c249f314235173c7213a7a4120a58b2a7034ece2
Ayman <aymanuddin1111.au@gmail.com>
2023-08-29 21:44:56 +0530

    added components

---
+++ src/app/pages/product/all-product/all-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-all-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './all-product.component.html',
+  styleUrls: ['./all-product.component.scss']
+})
+export class AllProductComponent {
+
+}
