ebdcaa9efba3130711b1b4e16b013851fe75bdb1
ayman <aymanuddin.au@pacewisdom.com>
2023-09-07 15:54:21 +0530

    product card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxSplideModule } from 'ngx-splide';
+import { ProductCardComponent } from 'src/app/layouts/product-card/product-card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NgxSplideModule],
+  imports: [CommonModule, NgxSplideModule, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
