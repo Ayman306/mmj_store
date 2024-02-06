23779ece9d2aa32c5213d9f5fcba371296cefa29
Ayman <aymanuddin1111.au@gmail.com>
2024-02-06 23:07:54 +0530

    search modal fix

--- src/app/shared/model/search-model/search-model.component.ts
+++ src/app/shared/model/search-model/search-model.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogRef } from '@angular/material/dialog';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-search-model',
@@ -9,6 +16,12 @@ import { MatDialogRef } from '@angular/material/dialog';
   imports: [CommonModule, MatIconModule],
   templateUrl: './search-model.component.html',
   styleUrls: ['./search-model.component.scss'],
+  animations: [
+    trigger('slideInFromRight', [
+      state('void', style({ transform: 'translate(0,-100%)', width: '100vw' })),
+      transition('void => *', animate('0.5s ease-in-out')),
+    ]),
+  ],
 })
 export class SearchModelComponent {
   constructor(public dialogRef: MatDialogRef<SearchModelComponent>) {}
