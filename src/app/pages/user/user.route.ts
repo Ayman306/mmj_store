ad6c27a4c91142e878d78387ca647213b6bb6001
Ayman <aymanuddin1111.au@gmail.com>
2024-02-18 11:50:22 +0530

    Update: wishlist route

--- src/app/pages/user/user.route.ts
+++ src/app/pages/user/user.route.ts
@@ -10,9 +10,5 @@ export default [
     path: 'profile',
     loadComponent: () =>
       import('./profile/profile.component').then((c) => c.ProfileComponent),
-  },
-  {
-    path: 'wishlist',
-    loadComponent:()=> import('./wishlist/wishlist.component').then((c)=>c.WishlistComponent),
   }
 ] as Routes;
