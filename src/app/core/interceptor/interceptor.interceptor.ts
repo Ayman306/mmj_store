9c187afc6753abeed4ae455b2025f086a9996e80
Ayman <aymanuddin1111.au@gmail.com>
2024-02-24 04:23:55 +0530

    Feature: Interceptor functionlaity handeled

--- src/app/core/interceptor/interceptor.interceptor.ts
+++ src/app/core/interceptor/interceptor.interceptor.ts
@@ -17,24 +17,37 @@ export class InterceptorInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const jwtToken:any = localStorage.getItem('Token') || '';
+    const jwtTokenObj:any =  localStorage.getItem('Token') || '';
+    const jwtToken= JSON.parse(jwtTokenObj) ;
+    const accessToken = jwtToken.access_token
     let refreshToken = '';
 
-    if (jwtToken) {
+    if (accessToken) {
       refreshToken = jwtToken.refresh_token
-      const clonedRequest = request.clone({
+      let clonedRequest = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${jwtToken}`
+          Authorization: `${accessToken}`
         }
       });
 
       return next.handle(clonedRequest).pipe(
         catchError((error) => {
-          if (error.status === 403 && refreshToken) {
+          if (error.status === 403 ) {
             // Call the refresh token API with the refresh token
             // Implement your refresh token logic here
             this.authService.refreshToken({refresh_token:refreshToken}).subscribe((res)=>{
-              this.authService.setToken(res);
+              this.authService.setToken(
+                {
+                  access_token: res.data,
+                  refresh_token: refreshToken
+                }
+              );
+               clonedRequest = request.clone({
+                setHeaders: {
+                  Authorization: `${res.data}`,
+                },
+              });
+              return next.handle(request);
             })
           }
           return throwError(error);
